923c01e0ac751f1d56feabd3ad0142148edc197a
Wisarut Phattanaphong <wisarut.ae@gmail.com>
2022-05-17 12:29:22 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# html-css-a4-template
+
+## using css and css framework like (Bootstrap)
+
+### References
+
+1. https://gist.github.com/dermotmcguire/97c4f7ec26411add138c
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HTML-CSS-REPORT</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous" media="screen">
+  <link href="assets/css/report.css" rel="stylesheet">
+</head>
+
+<body>
+  <nav class="navbar fixed-top bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand">Title</a>
+      <div class="d-flex">
+        <button class="btn btn-success me-2" type="button">Print</button>
+        <button class="btn btn-info" type="button">Download</button>
+      </div>
+    </div>
+  </nav>
+  <page size="A4"></page>
+  <page size="A4"></page>
+  <!-- <page size="A4" layout="landscape"></page>
+  <page size="A5"></page>
+  <page size="A5" layout="landscape"></page>
+  <page size="A3"></page>
+  <page size="A3" layout="landscape"></page> -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+
+</html>
