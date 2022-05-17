522bbdcd642fa10618325e935af26deb522965bf
Wisarut Phattanaphong <wisarut.ae@gmail.com>
2022-05-17 17:13:01 +0700

    update

--- index.php
+++ index.php
@@ -6,11 +6,15 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>HTML-CSS-REPORT</title>
+  <!-- Normalize or reset CSS with your favorite library -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
+  <!-- Bootstrap 5 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous" media="screen">
   <link href="assets/css/report.css" rel="stylesheet">
 </head>
 
 <body>
+
   <nav class="navbar fixed-top bg-light">
     <div class="container-fluid">
       <a class="navbar-brand">Title</a>
@@ -20,9 +24,25 @@
       </div>
     </div>
   </nav>
+
+  <div class="page-margin-top"></div>
+
+  <page size="A4">
+    <article>This is an A4 document.</article>
+    <?php
+    for ($i = 1; $i <= 25; $i++) {
+      echo '<p>' . $i . '</p>';
+    }
+    ?>
+  </page>
+  <page size="A4">
+    <article>This is an A4 document.</article>
+  </page>
+
+  <!-- <div class="page-margin-top"></div>
   <page size="A4"></page>
   <page size="A4"></page>
-  <!-- <page size="A4" layout="landscape"></page>
+  <page size="A4" layout="landscape"></page>
   <page size="A5"></page>
   <page size="A5" layout="landscape"></page>
   <page size="A3"></page>
